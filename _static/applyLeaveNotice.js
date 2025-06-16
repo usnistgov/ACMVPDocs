0aa488c4da6d3449f52615727bcad2e97da3761b
Joshua Klosterman <joshua.klosterman@nist.gov>
2025-06-16 15:57:49 -0400

    Leave Notice Fix

--- _static/applyLeaveNotice.js
+++ _static/applyLeaveNotice.js
@@ -2,21 +2,22 @@
   $(document).ready(function () {
     // Mark external (non-nist.gov) A tags with class "external"
     // If the address starts with https and ends with nist.gov
-    var re_nist = new RegExp('^https?:\/\/((^\/)*\.)*nist\\.gov(\/|$)'); 
+    var re_nist = new RegExp('^https?:\/\/((^\/)*\.)*nist\.gov(\/|$)'); 
     // Regex to find address that start with https
     var re_absolute_address = new RegExp('^((https?:)?\/\/)');
     $("a").each(function () {
       var url = $(this).attr('href');
+     // console.log("URL Test: ", re_nist.test(url), " ", re_absolute_address.test(url), " - ", url);
       if (re_nist.test(url) || !re_absolute_address.test(url)) {
         $(this).addClass('local');
       } else {
         //This a href appears to be external, so tag it
-        $(this).addClass('external');
+        $(this).addClass('nistExternal');
       }
     });
     // Add leaveNotice to external A elements
-    $('a.external').leaveNotice({
-      siteName: "Implementing a Zero Trust Architecture Project"
+    $('a.nistExternal').leaveNotice({
+      siteName: "Automation of the NIST Cryptographic Module Validation Program"
     });
   });
 })(jQuery);
